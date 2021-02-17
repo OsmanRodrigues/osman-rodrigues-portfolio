c9d6a38252dcda4d7e001d2abf0734ae559f81f8
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-02-16 22:05:40 -0300

    temporary test disabled

--- src/mocks/projects-mock.ts
+++ src/mocks/projects-mock.ts
@@ -1,13 +1,5 @@
 import { Project } from '@/models/project-model'
 
-const singleProject: Project = {
-  title: 'Metaliflix',
-  subtitle: 'Web responsivo',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare rutrum condimentum. Quisque pellentesque nisl vel risus venenatis sagittis. Sed ut ante facilisis, imperdiet sem vel, sollicitudin mauris. Sed sed egestas orci, eget imperdiet libero. In massa tellus, congue quis ex eget, porta mollis purus. Integer vehicula augue neque, fringilla iaculis libero vehicula nec. Proin ultricies orci ligula, id venenatis orci mattis non. Nam non aliquet elit. Mauris est ante, dapibus at diam sit amet, pretium commodo nisl. Quisque aliquet, ligula quis auctor congue, leo nunc auctor mi, ac pulvinar neque metus non lectus. Aliquam eleifend ut urna nec gravida.',
-  url: 'http://ocmr.metaliflix.surge.sh/'
-}
-
 export const projects: Project[] = [
   {
     title: 'Rappi4B',
